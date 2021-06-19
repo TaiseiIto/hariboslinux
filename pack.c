cb7f6096d330479954c6413fd18e127c78780685
TaiseiIto <taisei1212@outlook.jp>
2021-06-19 06:32:16 +0000

    packにコマンドライン引数を表示させる #2

--- pack.c
+++ pack.c
@@ -6,8 +6,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
+	//check argv
+	for(int argc_i = 0; argc_i < argc; argc_i++)printf("argv[%d] : %s\n", argc_i, argv[argc_i]);
 	return EXIT_SUCCESS;
 }
 
