a7a5b2ba5f9c723708d851a52e2cc8c14270b55b
TaiseiIto <taisei1212@outlook.jp>
2021-06-19 08:05:30 +0000

    packにboot_sector.binを読み込ませてる #2

--- pack.c
+++ pack.c
@@ -6,20 +6,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct
+{
+	unsigned char jump_instructions[3];
+
+	// The string doesn't end with '\0'.
+	// Margin is filled with spaces.
+	char product_name[8];
+
+	unsigned short num_of_bytes_per_sector;
+	unsigned char num_of_sectors_per_cluster;
+
+	// Number of reserved sectors.
+	// The boot record sectors are included in this value.
+	// After the sectors, FAT sectors begin.
+	unsigned short num_of_reserved_sectors;
+
+	unsigned char num_of_FATs;
+	unsigned short num_of_root_directory_entries;
+	unsigned short num_of_sectors_in_disk;
+	unsigned char media_type;
+	unsigned short num_of_sectors_per_FAT;
+	unsigned short num_of_sectors_per_track;
+	unsigned short num_of_heads;
+	unsigned int num_of_hidden_sectors;
+	unsigned int large_num_of_sectors_in_disk;
+	unsigned char drive_number;
+	unsigned char reserved;
+
+	// If it's 0x29, it's bootable;
+	unsigned char boot_signature;
+
+	unsigned int volume_serial_number;
+
+	// The strings doesn't end with '\0'.
+	// Margin is filled with spaces.
+	char volume_label[11];
+	char file_system_name[8];
+} __attribute__((packed)) BootSector;
+
 int main(int argc, char const * const * const argv)
 {
+	// related to command line arguments
 	// Haribos Linux floppy disk raw image file name
 	char const *output_file_name;
 	// Haribos Linux floppy disk boot sector binary file name
 	char const *boot_sector_file_name;
 	// file names included in the floppy disk
 	char const * const *input_file_names;
-	// Haribos Linux floppy disk boot sector binary file
-	FILE *boot_sector_file;
 	// num of [FILE] ...
 	unsigned int num_of_input_files;
 	// "pack", "OUTPUT" and "BOOOTSECTOR"
 	unsigned int const num_of_necessary_args = 3;
+
+	// related to boot sector
+	// read from boot_sector_file
+	BootSector *boot_sector;
+	void *boot_sector_image;
+	unsigned int const boot_sector_size = 0x00000200;
+	// Haribos Linux floppy disk boot sector binary file
+	FILE *boot_sector_file;
+
 	// check argc
 	if(argc < num_of_necessary_args)
 	{
@@ -42,6 +89,19 @@ int main(int argc, char const * const * const argv)
 		fprintf(stderr, "Can't open %s\n", boot_sector_file_name);
 		return EXIT_FAILURE;
 	}
+	if((boot_sector_image = malloc(boot_sector_size)) == NULL)
+	{
+		fprintf(stderr, "Can't alloc boot sector image!\n");
+		return EXIT_FAILURE;
+	}
+	if(fread(boot_sector_image, 1, boot_sector_size, boot_sector_file) < boot_sector_size)
+	{
+		fprintf(stderr, "Can't read %s\n", boot_sector_file_name);
+		return EXIT_FAILURE;
+	}
+	boot_sector = boot_sector_image;
+	printf("jump instructions[0] : %#04X\n", boot_sector->jump_instructions[0]);
+	free(boot_sector_image);
 	if((fclose(boot_sector_file)) == EOF)
 	{
 		fprintf(stderr, "Can't close %s\n", boot_sector_file_name);
