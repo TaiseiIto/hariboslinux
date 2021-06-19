94c477f45b4451fbe02dd69e754200a2b6692406
TaiseiIto <taisei1212@outlook.jp>
2021-06-19 06:26:24 +0000

    packの仕様を定めた #2

--- pack.c
+++ pack.c
@@ -1,3 +1,8 @@
+// Usage: pack OUTPUT BOOTSECTOR [FILE] ...
+//
+// This program outputs Haribos Linux floppy disk raw image file named by the argument OUTPUT.
+// The output includes the files specified by the arguments [FILE] ...
+
 #include <stdio.h>
 #include <stdlib.h>
 
