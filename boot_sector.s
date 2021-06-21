680a7ef45e4cec35f88d58c45aad847fd7622e8c
TaiseiIto <taisei1212@outlook.jp>
2021-06-21 21:58:58 +0900

    コード整形

--- boot_sector.s
+++ boot_sector.s
@@ -1,30 +1,30 @@
 # bibliography
 # https://wiki.osdev.org/FAT
 
-.text
-.code16				# real mode
+	.text
+	.code16			# real mode
 stack:				# bottom of stack
 	jmp	hello_world
 	nop
-.ascii	"HARIBOTE"		# OEM identifier
-.word	512			# The number of bytes per sector
-.byte	1			# Number of sectors per cluster
-.word	1			# Number of reserved sectors. The boot record sectors are included in this value. After the sectors, FAT sectors begin.
-.byte	2			# Number of FATs
-.word	224			# Number of root directory entries
-.word	2880			# Number of sectors in the logical volume
-.byte	0xf0			# Media dexcriptor type
-.word	9			# Number of sectors per FAT
-.word	18			# Number of sectors per track
-.word	2			# Number of heads or sides on the storage media
-.long	0			# Number of hidden sectors
-.long	2880			# Number of sectors in the logical volume
-.byte	0			# Drive number
-.byte	0			# Reserved
-.byte	0x29			# boot sector signature
-.long	0xffffffff		# volume serial number
-.ascii	"HARIBOTEOS "		# volume label
-.ascii	"FAT12   "		# FAT file system type
+	.ascii	"HARIBOTE"	# OEM identifier
+	.word	512		# The number of bytes per sector
+	.byte	1		# Number of sectors per cluster
+	.word	1		# Number of reserved sectors. The boot record sectors are included in this value. After the sectors, FAT sectors begin.
+	.byte	2		# Number of FATs
+	.word	224		# Number of root directory entries
+	.word	2880		# Number of sectors in the logical volume
+	.byte	0xf0		# Media dexcriptor type
+	.word	9		# Number of sectors per FAT
+	.word	18		# Number of sectors per track
+	.word	2		# Number of heads or sides on the storage media
+	.long	0		# Number of hidden sectors
+	.long	2880		# Number of sectors in the logical volume
+	.byte	0		# Drive number
+	.byte	0		# Reserved
+	.byte	0x29		# boot sector signature
+	.long	0xffffffff	# volume serial number
+	.ascii	"HARIBOTEOS "	# volume label
+	.ascii	"FAT12   "	# FAT file system type
 hello_world:
 	movw	$0,	%ax
 	movw	%ax,	%ss
@@ -45,5 +45,5 @@ fin:
 	hlt
 	jmp	fin
 msg:
-.string	"Hello, World!\r\n"
+	.string	"Hello, World!\r\n"
 
