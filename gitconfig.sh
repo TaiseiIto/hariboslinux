2f4f3ac09ede2c8089d416fcb32a2f4b71866800
TaiseiIto <taisei1212@outlook.jp>
2021-07-04 15:53:39 +0900

    gitの設定

--- gitconfig.sh
+++ gitconfig.sh
@@ -6,4 +6,10 @@ echo -n "Your GitHub name:"
 read name
 git config --global user.email $email
 git config --global user.name $name
+echo /url/ > .edcmd
+echo .s/github.com/${name}@github.com/ >> .edcmd
+echo w >> .edcmd
+echo q >> .edcmd
+ed .git/config < .edcmd
+rm .edcmd
 
