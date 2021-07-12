59e6e57fc665f1c79e601b71d8c149d5e56021c9
TaiseiIto <taisei1212@outlook.jp>
2021-07-12 21:30:56 +0900

    Hello, entry32.bin!表示成功 #11

--- src/entry32.s
+++ src/entry32.s
@@ -13,12 +13,35 @@ main:
 0:
 	pushl	%ebp
 	movl	%esp,	%ebp
-	pushl	0x00000041
-	call	putchar_serial
+	pushl	$hello_message
+	call	print_serial
 	leave
 0:					# halt loop
 	hlt
 	jmp	0b
+
+				# // print string to serial port COM1
+print_serial:			# void print_serial(char *string);
+0:
+	pushl	%ebp
+	movl	%esp,	%ebp
+	pushl	%esi
+	subl	$0x00000004,%esp
+	movl	0x08(%ebp),%esi
+1:				# put loop
+	xorl	%eax,	%eax
+	movb	(%esi),	%al
+	cmpb	$0x00,	%al
+	je	2f
+	movl	%eax,	(%esp)
+	call	putchar_serial
+	incl	%esi
+	jmp	1b
+2:
+	addl	$0x00000004,%esp
+	popl	%esi
+	leave
+	ret
 
 				# // print a character to console
 putchar_serial:			# void putchar_serial(char c);
@@ -39,3 +62,6 @@ putchar_serial:			# void putchar_serial(char c);
 	leave
 	ret
 
+	.data
+hello_message:
+	.string "Hello, entry32.bin!\n"
