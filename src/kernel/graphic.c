81a3f1ec8d73735cd13a7ffd6d9b0fbb896351b5
TaiseiIto <taisei1212@outlook.jp>
2021-07-22 15:08:16 +0900

    バグ解消 #15

--- src/kernel/graphic.c
+++ src/kernel/graphic.c
@@ -14,14 +14,16 @@ void init_palette(void)
 	outb(0x03c8, 0x00);
 	for(unsigned char color_number = 0x00; color_number < 0x06 * 0x06 * 0x06; color_number++)
 	{
+		unsigned char color_number_destroyable;
 		unsigned char red;
 		unsigned char green;
 		unsigned char blue;
-		red = 0x33 * (color_number % 6);
-		color_number /= 6;
-		green = 0x33 * (color_number % 6);
-		color_number /= 6;
-		blue = 0x33 * (color_number % 6);
+		color_number_destroyable = color_number;
+		red = 0x33 * (color_number_destroyable % 6);
+		color_number_destroyable /= 6;
+		green = 0x33 * (color_number_destroyable % 6);
+		color_number_destroyable /= 6;
+		blue = 0x33 * (color_number_destroyable % 6);
 		outb(0x03c9, red);
 		outb(0x03c9, green);
 		outb(0x03c9, blue);
