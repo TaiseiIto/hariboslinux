1c9ce45d29cd2c63cbc1a3d8256c03b3f3f094ea
TaiseiIto <taisei1212@outlook.jp>
2021-08-22 18:51:23 +0900

    BootInformation構造体修正 #29

--- src/kernel/boot.h
+++ src/kernel/boot.h
@@ -4,14 +4,11 @@
 typedef struct
 {
 	unsigned short memory_size;	// MiB
-	unsigned short screen_width;
-	unsigned short screen_height;
-	unsigned char bits_per_pixel;
 	unsigned char keyboard_state;
 	unsigned char last_loaded_cylinder;
 	unsigned char last_loaded_head;
 	unsigned char last_loaded_sector;
-} BootInformation;
+} __attribute__((packed)) BootInformation;
 
 #endif
 
--- src/kernel/main.c
+++ src/kernel/main.c
@@ -24,32 +24,31 @@ void main(void)
 	init_mouse();
 	init_screen();
 	new_line_serial_polling();
-	for(unsigned short y = 0; y < 0x100; y++)for(unsigned short x = 0; x < 0x100; x++)
+	for(unsigned short y = 0x0100; y < 0x0200; y++)for(unsigned short x = 0; x < 0x100; x++)
 	{
 		color.red = (unsigned char)x;
-		color.green = (unsigned char)y;
+		color.green = (unsigned char)(y - 0x0100);
 		color.blue = 0;
 		put_dot(x, y, color);
 	}
 	color.red = 0xff;
 	color.green = 0x00;
 	color.blue = 0x00;
-	fill_box(0x0100, 0x0000, 0x0100, 0x0100, color);
+	fill_box(0x0100, 0x0100, 0x0100, 0x0100, color);
 	color.red = 0x00;
 	color.green = 0xff;
 	color.blue = 0x00;
-	fill_box(0x0200, 0x0000, 0x0100, 0x0100, color);
+	fill_box(0x0200, 0x0100, 0x0100, 0x0100, color);
 	color.red = 0x00;
 	color.green = 0x00;
 	color.blue = 0xff;
-	fill_box(0x0300, 0x0000, 0x0100, 0x0100, color);
+	fill_box(0x0300, 0x0100, 0x0100, 0x0100, color);
 	background_color.red = 0x00;
 	background_color.green = 0x00;
 	background_color.blue = 0x00;
 	foreground_color.red = 0xff;
 	foreground_color.green = 0xff;
 	foreground_color.blue = 0xff;
-	printf_screen(0, 0x0100, foreground_color, background_color, "Hello, World!\nHello, hariboslinux!\n");
 	while(1)hlt();
 }
 
