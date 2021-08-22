11b2de9ece7be0b3ac1425a52f73e83606d82a98
TaiseiIto <taisei1212@outlook.jp>
2021-08-22 15:19:35 +0900

    一般保護例外解消 #29

--- src/kernel/graphic.c
+++ src/kernel/graphic.c
@@ -6,42 +6,6 @@
 
 #define TAB_LENGTH 4
 
-typedef struct
-{
-	unsigned short attributes;
-	unsigned char window_a;
-	unsigned char window_b;
-	unsigned short granularity;
-	unsigned short window_size;
-	unsigned short segment_a;
-	unsigned short segment_b;
-	unsigned int win_func_ptr;
-	unsigned short pitch;
-	unsigned short width;
-	unsigned short height;
-	unsigned char w_char;
-	unsigned char y_char;
-	unsigned char planes;
-	unsigned char bits_per_pixel;
-	unsigned char banks;
-	unsigned char memory_model;
-	unsigned char bank_size;
-	unsigned char image_pages;
-	unsigned char reserved;
-	unsigned char red_mask;
-	unsigned char red_position;
-	unsigned char green_mask;
-	unsigned char green_position;
-	unsigned char blue_mask;
-	unsigned char blue_position;
-	unsigned char reserved_mask;
-	unsigned char reserved_position;
-	unsigned char direct_color_attributes;
-	unsigned int frame_buffer;
-	unsigned int off_screen_mem_off;
-	unsigned int off_screen_mem_size;
-} __attribute__((packed)) VideoInformation;
-
 VideoInformation video_information;
 
 // fill box
