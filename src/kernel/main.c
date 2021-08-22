5eb91ffe7c743086ccd72d5a27dd866e3c018191
TaiseiIto <taisei1212@outlook.jp>
2021-08-22 19:05:11 +0900

    BootInformation構造体の取得に成功 #29

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -9,8 +9,11 @@
 #include "serial.h"
 #include "stdio.h"
 
+BootInformation get_boot_information(void);
+
 void main(void)
 {
+	BootInformation boot_information;
 	Color background_color;
 	Color color;
 	Color foreground_color;
@@ -23,7 +26,18 @@ void main(void)
 	init_keyboard();
 	init_mouse();
 	init_screen();
-	new_line_serial_polling();
+	boot_information = get_boot_information();
+	background_color.red = 0x00;
+	background_color.green = 0x00;
+	background_color.blue = 0x00;
+	foreground_color.red = 0xff;
+	foreground_color.green = 0xff;
+	foreground_color.blue = 0xff;
+	printf_screen(0x0000, 0 * CHAR_HEIGHT, foreground_color, background_color, "memory size = %#06X MiB", boot_information.memory_size);
+	printf_screen(0x0000, 1 * CHAR_HEIGHT, foreground_color, background_color, "keyboard state = %#04X", boot_information.keyboard_state);
+	printf_screen(0x0000, 2 * CHAR_HEIGHT, foreground_color, background_color, "last loaded cylinder = %#04X", boot_information.last_loaded_cylinder);
+	printf_screen(0x0000, 3 * CHAR_HEIGHT, foreground_color, background_color, "last loaded head = %#04X", boot_information.last_loaded_head);
+	printf_screen(0x0000, 4 * CHAR_HEIGHT, foreground_color, background_color, "last loaded sector = %#04X", boot_information.last_loaded_sector);
 	for(unsigned short y = 0x0100; y < 0x0200; y++)for(unsigned short x = 0; x < 0x100; x++)
 	{
 		color.red = (unsigned char)x;
@@ -43,12 +57,14 @@ void main(void)
 	color.green = 0x00;
 	color.blue = 0xff;
 	fill_box(0x0300, 0x0100, 0x0100, 0x0100, color);
-	background_color.red = 0x00;
-	background_color.green = 0x00;
-	background_color.blue = 0x00;
-	foreground_color.red = 0xff;
-	foreground_color.green = 0xff;
-	foreground_color.blue = 0xff;
+	new_line_serial_polling();
 	while(1)hlt();
+}
+
+BootInformation get_boot_information(void)
+{
+	BootInformation boot_information;
+	reads(boot_information_segment_selector, (void *)0x00000000, (void *)&boot_information, sizeof(boot_information));
+	return boot_information;
 }
 
