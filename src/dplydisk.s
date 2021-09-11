04f2a4ae0f5b99ddd04ca499f86b5a4899c9cd3f
TaiseiIto <taisei1212@outlook.jp>
2021-09-11 15:49:59 +0900

    dplydisk.binで一文字出力 #28

--- src/dplydisk.s
+++ src/dplydisk.s
@@ -9,14 +9,46 @@
 	.include	"global.s"
 
 	.globl	main
+	.globl	putchar_serial
 
-	.type	main,	@function
+	.type	main,		@function
+	.type	putchar_serial,	@function
 
+	.code32
 	.text
 main:
 0:
+	pushl	%ebp
+	movl	%esp,	%ebp
+	subl	$0x00000004,%esp
+	movl	$0x00000041,%edx
+	movl	%edx,	(%esp)
+	call	putchar_serial
+1:
+	addl	$0x00000004,%esp
+	leave
+2:
 	hlt
-	jmp	0b
+	jmp	2b
+
+				# // print a character to console
+putchar_serial:			# void putchar_serial(char c);
+0:
+	pushl	%ebp
+	movl	%esp,	%ebp
+1:				# wait for device
+	movw	$com1,	%dx
+	addw	$0x0005,%dx
+	inb	%dx,	%al
+	andb	$0x20,	%al
+	jz	1b
+2:				# send the character
+	movb	0x08(%ebp),%al
+	movw	$com1,	%dx
+	outb	%al,	%dx
+3:
+	leave
+	ret
 
 	.data
 	.align	0x0200
