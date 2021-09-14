9ce26d8de84cc074d0cb7c1b26144c2b2e7eb45d
TaiseiIto <taisei1212@outlook.jp>
2021-09-14 22:01:17 +0900

    とりあえず0を返す関数として作成した #31

---
+++ src/kernel/arithmetic.c
@@ -0,0 +1,26 @@
+#include "arithmetic.h"
+
+long long int __divdi3(long long int a, long long int b)
+{
+	long long int result = 0;
+	return result;
+}
+
+long long int __moddi3(long long int a, long long int b)
+{
+	long long int result = 0;
+	return result;
+}
+
+unsigned long long int __udivdi3(unsigned long long int a, unsigned long long int b)
+{
+	unsigned long long int result = 0;
+	return result;
+}
+
+unsigned long long int __umoddi3(unsigned long long int a, unsigned long long int b)
+{
+	unsigned long long int result = 0;
+	return result;
+}
+
---
+++ src/kernel/arithmetic.h
@@ -0,0 +1,10 @@
+#ifndef _ARITHMETIC_H_
+#define _ARITHMETIC_H_
+
+long long int __divdi3(long long int a, long long int b);
+long long int __moddi3(long long int a, long long int b);
+unsigned long long int __udivdi3(unsigned long long int a, unsigned long long int b);
+unsigned long long int __umoddi3(unsigned long long int a, unsigned long long int b);
+
+#endif
+
