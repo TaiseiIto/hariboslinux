b0b8d21d1a263842d57d2ca146eec2fd7ff4c18c
TaiseiIto <taisei1212@outlook.jp>
2021-10-03 12:59:23 +0900

    キーマップ作成中 #38

--- src/kernel/keyboard.c
+++ src/kernel/keyboard.c
@@ -18,7 +18,7 @@ KeyboardTransmission last_keyboard_transmission;
 
 char get_character(unsigned char signal)
 {
-	const char english_no_caps_lock_no_shift_character_map[0x100] = 
+	const char english_no_caps_lock_no_shift_character_map[0x80] = 
 	{
 		'\0',	// 0x00
 		'\0',	// 0x01 KEY_ESC
@@ -147,137 +147,141 @@ char get_character(unsigned char signal)
 		'\0',	// 0x7c
 		'\0',	// 0x7d
 		'\0',	// 0x7e
-		'\0',	// 0x7f
-		'\0',	// 0x80
-		'\0',	// 0x81
-		'\0',	// 0x82
-		'\0',	// 0x83
-		'\0',	// 0x84
-		'\0',	// 0x85
-		'\0',	// 0x86
-		'\0',	// 0x87
-		'\0',	// 0x88
-		'\0',	// 0x89
-		'\0',	// 0x8a
-		'\0',	// 0x8b
-		'\0',	// 0x8c
-		'\0',	// 0x8d
-		'\0',	// 0x8e
-		'\0',	// 0x8f
-		'\0',	// 0x90
-		'\0',	// 0x91
-		'\0',	// 0x92
-		'\0',	// 0x93
-		'\0',	// 0x94
-		'\0',	// 0x95
-		'\0',	// 0x96
-		'\0',	// 0x97
-		'\0',	// 0x98
-		'\0',	// 0x99
-		'\0',	// 0x9a
-		'\0',	// 0x9b
-		'\0',	// 0x9c
-		'\0',	// 0x9d
-		'\0',	// 0x9e
-		'\0',	// 0x9f
-		'\0',	// 0xa0
-		'\0',	// 0xa1
-		'\0',	// 0xa2
-		'\0',	// 0xa3
-		'\0',	// 0xa4
-		'\0',	// 0xa5
-		'\0',	// 0xa6
-		'\0',	// 0xa7
-		'\0',	// 0xa8
-		'\0',	// 0xa9
-		'\0',	// 0xaa
-		'\0',	// 0xab
-		'\0',	// 0xac
-		'\0',	// 0xad
-		'\0',	// 0xae
-		'\0',	// 0xaf
-		'\0',	// 0xb0
-		'\0',	// 0xb1
-		'\0',	// 0xb2
-		'\0',	// 0xb3
-		'\0',	// 0xb4
-		'\0',	// 0xb5
-		'\0',	// 0xb6
-		'\0',	// 0xb7
-		'\0',	// 0xb8
-		'\0',	// 0xb9
-		'\0',	// 0xba
-		'\0',	// 0xbb
-		'\0',	// 0xbc
-		'\0',	// 0xbd
-		'\0',	// 0xbe
-		'\0',	// 0xbf
-		'\0',	// 0xc0
-		'\0',	// 0xc1
-		'\0',	// 0xc2
-		'\0',	// 0xc3
-		'\0',	// 0xc4
-		'\0',	// 0xc5
-		'\0',	// 0xc6
-		'\0',	// 0xc7
-		'\0',	// 0xc8
-		'\0',	// 0xc9
-		'\0',	// 0xca
-		'\0',	// 0xcb
-		'\0',	// 0xcc
-		'\0',	// 0xcd
-		'\0',	// 0xce
-		'\0',	// 0xcf
-		'\0',	// 0xd0
-		'\0',	// 0xd1
-		'\0',	// 0xd2
-		'\0',	// 0xd3
-		'\0',	// 0xd4
-		'\0',	// 0xd5
-		'\0',	// 0xd6
-		'\0',	// 0xd7
-		'\0',	// 0xd8
-		'\0',	// 0xd9
-		'\0',	// 0xda
-		'\0',	// 0xdb
-		'\0',	// 0xdc
-		'\0',	// 0xdd
-		'\0',	// 0xde
-		'\0',	// 0xdf
-		'\0',	// 0xe0
-		'\0',	// 0xe1
-		'\0',	// 0xe2
-		'\0',	// 0xe3
-		'\0',	// 0xe4
-		'\0',	// 0xe5
-		'\0',	// 0xe6
-		'\0',	// 0xe7
-		'\0',	// 0xe8
-		'\0',	// 0xe9
-		'\0',	// 0xea
-		'\0',	// 0xeb
-		'\0',	// 0xec
-		'\0',	// 0xed
-		'\0',	// 0xee
-		'\0',	// 0xef
-		'\0',	// 0xf0
-		'\0',	// 0xf1
-		'\0',	// 0xf2
-		'\0',	// 0xf3
-		'\0',	// 0xf4
-		'\0',	// 0xf5
-		'\0',	// 0xf6
-		'\0',	// 0xf7
-		'\0',	// 0xf8
-		'\0',	// 0xf9
-		'\0',	// 0xfa
-		'\0',	// 0xfb
-		'\0',	// 0xfc
-		'\0',	// 0xfd
-		'\0',	// 0xfe
-		'\0'	// 0xff
+		'\0'	// 0x7f
 	};
-	return english_no_caps_lock_no_shift_character_map[signal];
+	const char english_no_caps_lock_shift_character_map[0x80] = 
+	{
+		'\0',	// 0x00
+		'\0',	// 0x01 KEY_ESC
+		'!',	// 0x02 KEY_1
+		'@',	// 0x03 KEY_2
+		'#',	// 0x04 KEY_3
+		'$',	// 0x05 KEY_4
+		'%',	// 0x06 KEY_5
+		'^',	// 0x07 KEY_6
+		'&',	// 0x08 KEY_7
+		'*',	// 0x09 KEY_8
+		'(',	// 0x0a KEY_9
+		')',	// 0x0b KEY_0
+		'_',	// 0x0c KEY_MINUS
+		'+',	// 0x0d KEY_EQUAL
+		'\b',	// 0x0e KEY_BACKSPACE
+		'\t',	// 0x0f KEY_TAB
+		'Q',	// 0x10 KEY_Q
+		'W',	// 0x11 KEY_W
+		'E',	// 0x12 KEY_E
+		'R',	// 0x13 KEY_R
+		'T',	// 0x14 KEY_T
+		'Y',	// 0x15 KEY_Y
+		'U',	// 0x16 KEY_U
+		'I',	// 0x17 KEY_I
+		'O',	// 0x18 KEY_O
+		'P',	// 0x19 KEY_P
+		'{',	// 0x1a KEY_LEFT_SQUARE_BRACKET
+		'}',	// 0x1b KEY_RIGHT_SQUARE_BRACKET
+		'\n',	// 0x1c KEY_ENTER
+		'\0',	// 0x1d KEY_CONTROL
+		'A',	// 0x1e KEY_A
+		'S',	// 0x1f KEY_S
+		'D',	// 0x20 KEY_D
+		'F',	// 0x21 KEY_F
+		'G',	// 0x22 KEY_G
+		'H',	// 0x23 KEY_H
+		'J',	// 0x24 KEY_J
+		'K',	// 0x25 KEY_K
+		'L',	// 0x26 KEY_L
+		':',	// 0x27 KEY_SEMICOLON
+		'\"',	// 0x28 KEY_QUOTE
+		'~',	// 0x29 KEY_BACKQUOTE
+		'\0',	// 0x2a KEY_LEFT_SHIFT
+		'|',	// 0x2b KEY_BACKSLASH
+		'Z',	// 0x2c KEY_Z
+		'X',	// 0x2d KEY_X
+		'C',	// 0x2e KEY_C
+		'V',	// 0x2f KEY_V
+		'B',	// 0x30 KEY_B
+		'N',	// 0x31 KEY_N
+		'M',	// 0x32 KEY_M
+		'<',	// 0x33 KEY_COMMA
+		'>',	// 0x34 KEY_PERIOD
+		'?',	// 0x35 KEY_SLASH
+		'\0',	// 0x36 KEY_RIGHT_SHIFT
+		'*',	// 0x37 KEY_PAD_ASTERISK
+		'\0',	// 0x38 KEY_ALT
+		' ',	// 0x39 KEY_SPACE
+		'\0',	// 0x3a KEY_CAPS_LOCK
+		'\0',	// 0x3b KEY_F1
+		'\0',	// 0x3c KEY_F2
+		'\0',	// 0x3d KEY_F3
+		'\0',	// 0x3e KEY_F4
+		'\0',	// 0x3f KEY_F5
+		'\0',	// 0x40 KEY_F6
+		'\0',	// 0x41 KEY_F7
+		'\0',	// 0x42 KEY_F8
+		'\0',	// 0x43 KEY_F9
+		'\0',	// 0x44 KEY_F10
+		'\0',	// 0x45 KEY_NUMBER_LOCK
+		'\0',	// 0x46 KEY_SCROLL_LOCK
+		'7',	// 0x47 KEY_PAD_7
+		'8',	// 0x48 KEY_PAD_8
+		'9',	// 0x49 KEY_PAD_9
+		'-',	// 0x4a KEY_PAD_MINUS
+		'4',	// 0x4b KEY_PAD_4
+		'5',	// 0x4c KEY_PAD_5
+		'6',	// 0x4d KEY_PAD_6
+		'+',	// 0x4e KEY_PAD_PLIS
+		'1',	// 0x4f KEY_PAD_1
+		'2',	// 0x50 KEY_PAD_2
+		'3',	// 0x51 KEY_PAD_3
+		'0',	// 0x52 KEY_PAD_0
+		'.',	// 0x53 KEY_PAD_PERIOD
+		'\0',	// 0x54
+		'\0',	// 0x55
+		'\0',	// 0x56
+		'\0',	// 0x57 KEY_F11
+		'\0',	// 0x58 KEY_F12
+		'\0',	// 0x59
+		'\0',	// 0x5a
+		'\0',	// 0x5b KEY_LEFT_SUPER
+		'\0',	// 0x5c KEY_RIGHT_SUPER
+		'\0',	// 0x5d
+		'\0',	// 0x5e
+		'\0',	// 0x5f
+		'\0',	// 0x60
+		'\0',	// 0x61
+		'\0',	// 0x62
+		'\0',	// 0x63
+		'\0',	// 0x64
+		'\0',	// 0x65
+		'\0',	// 0x66
+		'\0',	// 0x67
+		'\0',	// 0x68
+		'\0',	// 0x69
+		'\0',	// 0x6a
+		'\0',	// 0x6b
+		'\0',	// 0x6c
+		'\0',	// 0x6d
+		'\0',	// 0x6e
+		'\0',	// 0x6f
+		'\0',	// 0x70
+		'\0',	// 0x71
+		'\0',	// 0x72
+		'\0',	// 0x73
+		'\0',	// 0x74
+		'\0',	// 0x75
+		'\0',	// 0x76
+		'\0',	// 0x77
+		'\0',	// 0x78
+		'\0',	// 0x79
+		'\0',	// 0x7a
+		'\0',	// 0x7b
+		'\0',	// 0x7c
+		'\0',	// 0x7d
+		'\0',	// 0x7e
+		'\0'	// 0x7f
+	};
+	if(keyboard_flags & KEYBOARD_FLAG_SHIFT_KEY_PUSHED)return english_no_caps_lock_shift_character_map[signal & ~KEY_RELEASED];
+	else return english_no_caps_lock_no_shift_character_map[signal & ~KEY_RELEASED];
 }
 
 void decode_keyboard_interrupt(unsigned char signal)
