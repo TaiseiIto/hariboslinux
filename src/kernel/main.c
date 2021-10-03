5b682945e22041ddb749972546445cc16254f1f3
TaiseiIto <taisei1212@outlook.jp>
2021-10-03 10:38:43 +0900

    キーボードレイアウトの切り替えを実装 #38

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -89,12 +89,11 @@ void main(void)
 		if(event)switch(event->type)
 		{
 		case EVENT_TYPE_KEYBOARD_EVENT:
-			if(keyboard_flags & (KEYBOARD_FLAG_LAYOUT_ENGLISH | KEYBOARD_FLAG_LAYOUT_JAPANESE) != event->event_union.keyboard_event.flags & (KEYBOARD_FLAG_LAYOUT_ENGLISH | KEYBOARD_FLAG_LAYOUT_JAPANESE))
+			if((keyboard_flags & (KEYBOARD_FLAG_LAYOUT_ENGLISH | KEYBOARD_FLAG_LAYOUT_JAPANESE)) != (event->event_union.keyboard_event.flags & (KEYBOARD_FLAG_LAYOUT_ENGLISH | KEYBOARD_FLAG_LAYOUT_JAPANESE)))
 			{
 				if(event->event_union.keyboard_event.flags & KEYBOARD_FLAG_LAYOUT_ENGLISH)print_screen(0x0000, 0x0000 * CHAR_HEIGHT, foreground_color, background_color, "keyboard layout = English ");
 				else if(event->event_union.keyboard_event.flags & KEYBOARD_FLAG_LAYOUT_JAPANESE)print_screen(0x0000, 0x0000 * CHAR_HEIGHT, foreground_color, background_color, "keyboard layout = Japanese");
 			}
-			printf_screen(0x0000, 0x0000 * CHAR_HEIGHT, foreground_color, background_color, "keyboard flags = %#06x", event->event_union.keyboard_event.flags);
 			printf_screen(0x0000, 0x0001 * CHAR_HEIGHT, foreground_color, background_color, "keyboard event character = %c", event->event_union.keyboard_event.character);
 			keyboard_flags = event->event_union.keyboard_event.flags;
 			break;
