7134c7d3e6a3d3eb45144970983a0ba2c7b75aac
TaiseiIto <taisei1212@outlook.jp>
2021-10-09 14:22:52 +0900

    曜日修正 #39

--- src/kernel/rtc.h
+++ src/kernel/rtc.h
@@ -9,13 +9,13 @@ typedef struct _RTCInterrupt
 	unsigned char minute;
 	unsigned char hour;
 	unsigned char day_of_week;
-	#define RTC_MONDAY	0x01
-	#define RTC_TUESDAY	0x02
-	#define RTC_WEDNESDAY	0x03
-	#define RTC_THURSDAY	0x04
-	#define RTC_FRIDAY	0x05
-	#define RTC_SATURDAY	0x06
-	#define RTC_SUNDAY	0x07
+	#define RTC_SUNDAY	0x01
+	#define RTC_MONDAY	0x02
+	#define RTC_TUESDAY	0x03
+	#define RTC_WEDNESDAY	0x04
+	#define RTC_THURSDAY	0x05
+	#define RTC_FRIDAY	0x06
+	#define RTC_SATURDAY	0x07
 	unsigned char day_of_month;
 	unsigned char month;
 	unsigned char year;
