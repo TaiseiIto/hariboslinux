194510fcd75137b270326e1605ac490c1b4ad8a7
TaiseiIto <taisei1212@outlook.jp>
2021-10-09 21:22:52 +0900

    タイマ実装中 #36

--- src/kernel/timer.c
+++ src/kernel/timer.c
@@ -1,26 +1,63 @@
 #include "memory.h"
+#include "task.h"
 #include "timer.h"
 
-Timer *next_timer;
-unsigned long long tick_counter;
+Timer *next_estimated_timer;
+unsigned long long tick_count;
 
 Timer *create_timer(unsigned long long estimated_count, unsigned long long interval_count)
 {
-	return NULL;
+	Timer *new_timer = (Timer *)malloc(sizeof(*new_timer));
+	new_timer->estimated_count = tick_count + estimated_count;
+	new_timer->interval_count = interval_count;
+	cli_task();
+	if(next_estimated_timer)
+	{
+		Timer *previous_timer;
+		for(previous_timer = next_estimated_timer; previous_timer->next; previous_timer = previous_timer->next)if(new_timer->estimated_count < previous_timer->next->estimated_count)break;
+		if(previous_timer == next_estimated_timer && new_timer->estimated_count < next_estimated_timer->estimated_count)
+		{
+			new_timer->next = next_estimated_timer;
+			next_estimated_timer->previous = new_timer;
+			new_timer->previous = NULL;
+			next_estimated_timer = new_timer;
+		}
+		else
+		{
+			new_timer->next = previous_timer->next;
+			if(new_timer->next)new_timer->next->previous = new_timer;
+			new_timer->previous = previous_timer;
+			previous_timer->next = new_timer;
+		}
+	}
+	else
+	{
+		new_timer->previous = NULL;
+		new_timer->next = NULL;
+		next_estimated_timer = new_timer;
+	}
+	sti_task();
+	return new_timer;
 }
 
 void delete_timer(Timer *timer)
 {
+	cli_task();
+	if(next_estimated_timer == timer)next_estimated_timer = timer->next;
+	if(timer->previous)timer->previous->next = timer->next;
+	if(timer->next)timer->next->previous = timer->previous;
+	sti_task();
+	free(timer);
 }
 
 void init_timer(void)
 {
-	next_timer = NULL;
-	tick_counter = 0;
+	next_estimated_timer = NULL;
+	tick_count = 0;
 }
 
 void timers_tick(void)
 {
-	tick_counter++;
+	tick_count++;
 }
 
