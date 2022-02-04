6f5f5c8af7dc183bca1d5d0fe801eab83d137cfc
TaiseiIto <taisei1212@outlook.jp>
2022-02-04 22:01:39 +0900

    色の調整 #41

--- src/kernel/window.c
+++ src/kernel/window.c
@@ -1,44 +1,23 @@
 #include "memory.h"
 #include "window.h"
 
+#define EDGE_WIDTH 5
+
 Window *create_window(Sheet *background_sheet, short x, short y, unsigned short width, unsigned short height)
 {
-	Color background_color;
-	Color dark_limit_color;
-	Color light_limit_color;
-	Color semi_dark_limit_color;
-	Color semi_light_limit_color;
-	Color title_background_color;
+
+	//					//{red ,green, blue,alpha}
+	const Color background_color		= {0x80, 0x80, 0x80, 0xff};
+	const Color dark_limit_color		= {0x00, 0x00, 0x00, 0xff};
+	const Color light_limit_color		= {0xff, 0xff, 0xff, 0xff};
+	const Color semi_dark_limit_color	= {0x40, 0x40, 0x40, 0xff};
+	const Color semi_light_limit_color	= {0xc0, 0xc0, 0xc0, 0xff};
+	const Color title_background_color	= {0x00, 0x00, 0x80, 0xff};
 	Window *new_window;
-	// Define colors
-	background_color.red = 0x80;
-	background_color.green = 0x80;
-	background_color.blue = 0x80;
-	background_color.alpha = 0xff;
-	dark_limit_color.red = 0x20;
-	dark_limit_color.green = 0x20;
-	dark_limit_color.blue = 0x20;
-	dark_limit_color.alpha = 0xff;
-	light_limit_color.red = 0xe0;
-	light_limit_color.green = 0xe0;
-	light_limit_color.blue = 0xe0;
-	light_limit_color.alpha = 0xff;
-	semi_dark_limit_color.red = 0x40;
-	semi_dark_limit_color.green = 0x40;
-	semi_dark_limit_color.blue = 0x40;
-	semi_dark_limit_color.alpha = 0xff;
-	semi_light_limit_color.red = 0xc0;
-	semi_light_limit_color.green = 0xc0;
-	semi_light_limit_color.blue = 0xc0;
-	semi_light_limit_color.alpha = 0xff;
-	title_background_color.red = 0x00;
-	title_background_color.green = 0x00;
-	title_background_color.blue = 0xff;
-	title_background_color.alpha = 0xff;
 	// Create sheets
 	new_window = malloc(sizeof(*new_window));
 	new_window->root_sheet = create_sheet(background_sheet, x, y, width, height);
-	new_window->title_sheet = create_sheet(new_window->root_sheet, 6, 6, new_window->root_sheet->width - 12, 16);
+	new_window->title_sheet = create_sheet(new_window->root_sheet, EDGE_WIDTH, EDGE_WIDTH, new_window->root_sheet->width - 2 * EDGE_WIDTH, 16);
 	// Draw root sheet
 	fill_box_sheet(new_window->root_sheet, 0, 0, new_window->root_sheet->width - 1, 1, light_limit_color);
 	fill_box_sheet(new_window->root_sheet, 0, 1, 1, new_window->root_sheet->height - 2, light_limit_color);
@@ -49,6 +28,7 @@ Window *create_window(Sheet *background_sheet, short x, short y, unsigned short 
 	fill_box_sheet(new_window->root_sheet, new_window->root_sheet->width - 2, 1, 1, new_window->root_sheet->height - 3, semi_dark_limit_color);
 	fill_box_sheet(new_window->root_sheet, 0, new_window->root_sheet->height - 1, new_window->root_sheet->width, 1, dark_limit_color);
 	fill_box_sheet(new_window->root_sheet, new_window->root_sheet->width - 1, 0, 1, new_window->root_sheet->height - 1, dark_limit_color);
+	// Draw edges of children sheet of root sheet
 	for(Sheet *root_child = new_window->root_sheet->lowest_child; root_child; root_child = root_child->upper)
 	{
 		fill_box_sheet(new_window->root_sheet, root_child->x - 2, root_child->y - 2, root_child->width + 3, 1, semi_dark_limit_color);
