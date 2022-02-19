31bc737d3e3f53128c9504393b6cf5335c177413
TaiseiIto <taisei1212@outlook.jp>
2022-02-19 21:04:02 +0900

    Debug Zeller's congruence

--- src/kernel/rtc.c
+++ src/kernel/rtc.c
@@ -21,13 +21,15 @@ unsigned char status_register_b;
 // https://en.wikipedia.org/wiki/Zeller%27s_congruence
 unsigned char get_day_of_week(unsigned short year, unsigned char month, unsigned char day)
 {
-	unsigned int year_of_the_century = year % 100;
-	unsigned int zero_based_century = year / 100;
+	unsigned int year_of_the_century;
+	unsigned int zero_based_century;
 	if(month <= 2)
 	{
 		month += 12;
 		year--;
 	}
+	year_of_the_century = year % 100;
+	zero_based_century = year / 100;
 	return (day + 13 * (month + 1) / 5 + year_of_the_century + year_of_the_century / 4 + zero_based_century / 4 - 2 * zero_based_century) % 7;
 }
 
