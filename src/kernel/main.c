1c2614ec84f6e1372621e661f95213b73c79c102
TaiseiIto <taisei1212@outlook.jp>
2022-03-05 22:34:44 +0900

    refactoring

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -156,7 +156,7 @@ void main(void)
 	fill_box_sheet(translucent_red_sheet, 0, 0, translucent_red_sheet->width, translucent_red_sheet->height, translucent_red);
 	fill_box_sheet(translucent_green_sheet, 0, 0, translucent_green_sheet->width, translucent_green_sheet->height, translucent_green);
 	fill_box_sheet(translucent_blue_sheet, 0, 0, translucent_blue_sheet->width, translucent_blue_sheet->height, translucent_blue);
-	while(1)
+	while(true)
 	{
 		Event new_event;
 		Event const *event;
