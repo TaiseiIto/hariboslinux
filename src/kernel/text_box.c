e39b3d6ff9b3eff0e9b173377a70f14b05e35c8b
TaiseiIto <taisei1212@outlook.jp>
2022-03-06 20:55:19 +0900

    text box event procedure #53

--- src/kernel/text_box.c
+++ src/kernel/text_box.c
@@ -4,11 +4,36 @@
 
 TextBox *root_text_box = NULL;
 
+TextBox *get_text_box_from_sheet(Sheet *sheet);
+void *text_box_event_procedure(Sheet *sheet, struct _Event const *event);
+
+TextBox *get_text_box_from_sheet(Sheet *sheet)
+{
+	TextBox *candidate_text_box;
+	prohibit_switch_task();
+	candidate_text_box = root_text_box;
+	do
+	{
+		if(candidate_text_box->sheet == sheet)
+		{
+			allow_switch_task();
+			return candidate_text_box;
+		}
+		candidate_text_box = candidate_text_box->next;
+	} while(candidate_text_box != root_text_box);
+	ERROR(); // Text box is not found.
+	allow_switch_task();
+	return NULL;
+}
+
 TextBox *make_sheet_text_box(Sheet *sheet)
 {
 	TextBox *new_text_box = malloc(sizeof(*new_text_box));
 	printf_serial("Make sheet %p text box %p\n", sheet, new_text_box);
 	prohibit_switch_task();
+	new_text_box->sheet = sheet;
+	new_text_box->default_event_procedure = new_text_box->sheet->event_procedure;
+	new_text_box->sheet->event_procedure = text_box_event_procedure;
 	if(root_text_box)
 	{
 		new_text_box->next = root_text_box;
@@ -24,5 +49,11 @@ TextBox *make_sheet_text_box(Sheet *sheet)
 	}
 	allow_switch_task();
 	return new_text_box;
+}
+
+void *text_box_event_procedure(Sheet *sheet, struct _Event const *event)
+{
+	TextBox *text_box = get_text_box_from_sheet(sheet);
+	return text_box->default_event_procedure(sheet, event);
 }
 
--- src/kernel/text_box.h
+++ src/kernel/text_box.h
@@ -8,6 +8,7 @@ typedef struct _TextBox
 	Sheet *sheet;
 	struct _TextBox *next;
 	struct _TextBox *previous;
+	void *(*default_event_procedure)(struct _Sheet *sheet, struct _Event const *event);
 } TextBox;
 
 TextBox *make_sheet_text_box(Sheet *sheet);
