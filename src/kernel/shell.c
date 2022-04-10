ebf44f53f18d19009ad1139af4fe90cb253bdcee
TaiseiIto <taisei1212@outlook.jp>
2022-04-10 14:31:32 +0900

    execute only elf file #54

--- src/kernel/shell.c
+++ src/kernel/shell.c
@@ -170,20 +170,23 @@ void *execute_command(Shell *shell, char const *command)
 	argv = create_argv(command);
 	if(argv)
 	{
+		char *executable_file_name;
 		void *executable_file_binary;
 		// Count argc.
 		for(argc = 0; argv[argc]; argc++);
 		// Print argv.
 		for(unsigned int argv_index = 0; argv_index < argc; argv_index++)printf_shell(shell, "argv[%d] = \"%s\"\n", argv_index, argv[argv_index]);
 		// Load a file specified by argv[0].
-		executable_file_binary = load_file(argv[0]);
+		executable_file_name = create_format_char_array("%s.elf", argv[0]);
+		executable_file_binary = load_file(executable_file_name);
 		if(executable_file_binary)
 		{
 			printf_shell(shell, "%.*s", get_file_information(argv[0])->size, executable_file_binary);
 			free(executable_file_binary);
 		}
-		else printf_shell(shell, "Executable file \"%s\" is not found.\n", argv[0]);
-		// Discard argv.
+		else printf_shell(shell, "Executable file \"%s\" is not found.\n", executable_file_name);
+		// Discard file name and argv.
+		free(executable_file_name);
 		for(unsigned int argv_index = 0; argv_index < argc; argv_index++)free(argv[argv_index]);
 		free(argv);
 		return NULL;
