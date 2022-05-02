ad7b088b70adcd9225034453f84e4008c6f77f5b
TaiseiIto <taisei1212@outlook.jp>
2022-05-02 22:01:29 +0900

    system_call_write #57

--- src/kernel/system_call.c
+++ src/kernel/system_call.c
@@ -3,6 +3,12 @@
 #include "shell.h"
 #include "system_call.h"
 
+#define STDIN	0x00000000
+#define STDOUT	0x00000001
+#define STDERR	0x00000002
+
+unsigned int system_call_write(unsigned int file_descriptor, void const *buffer, size_t count);
+
 unsigned int system_call(unsigned int eax, unsigned int ebx, unsigned int ecx, unsigned int edx, unsigned int esi, unsigned int edi, unsigned int ebp)
 {
 	UNUSED_ARGUMENT(ecx);
@@ -10,15 +16,41 @@ unsigned int system_call(unsigned int eax, unsigned int ebx, unsigned int ecx, u
 	UNUSED_ARGUMENT(esi);
 	UNUSED_ARGUMENT(edi);
 	UNUSED_ARGUMENT(ebp);
-	#define SYSTEM_CALL_EAX_EXIT 0x0001
+	#define SYSTEM_CALL_EXIT	0x00000001
+	#define SYSTEM_CALL_WRITE	0x00000004
 	switch(eax)
 	{
-	case SYSTEM_CALL_EAX_EXIT:
+	case SYSTEM_CALL_EXIT:
 		exit_application(ebx, get_current_task()->task_status_segment.esp0);
 		break;
+	case SYSTEM_CALL_WRITE:
+		system_call_write(ebx, (void const *)ecx, (size_t)edx);
+		break;
 	default:
+		ERROR(); // Invalid eax
 		break;
 	}
 	return 0;
+}
+
+unsigned int system_call_write(unsigned int file_descriptor, void const *buffer, size_t count)
+{
+	Shell *shell = get_current_shell();
+	unsigned int counter = 0;
+	switch(file_descriptor)
+	{
+	case STDOUT:
+	case STDERR:
+		for(void const *reader = buffer; reader != buffer + count && *(char const *)reader; reader++)
+		{
+			put_char_shell(shell, *(char const *)reader);
+			counter++;
+		}
+		break;
+	default:
+		ERROR(); // Invalid file descriptor
+		break;
+	}
+	return counter;
 }
 
