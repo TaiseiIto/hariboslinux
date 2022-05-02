b36cb913ed667511ba5be8878c574d7b6a57813b
TaiseiIto <taisei1212@outlook.jp>
2022-05-03 08:59:30 +0900

    refactoring

--- src/kernel/system_call.c
+++ src/kernel/system_call.c
@@ -7,12 +7,11 @@
 #define STDOUT	0x00000001
 #define STDERR	0x00000002
 
+unsigned int system_call_exit(unsigned int return_value);
 unsigned int system_call_write(unsigned int file_descriptor, void const *buffer, size_t count);
 
 unsigned int system_call(unsigned int eax, unsigned int ebx, unsigned int ecx, unsigned int edx, unsigned int esi, unsigned int edi, unsigned int ebp)
 {
-	UNUSED_ARGUMENT(ecx);
-	UNUSED_ARGUMENT(edx);
 	UNUSED_ARGUMENT(esi);
 	UNUSED_ARGUMENT(edi);
 	UNUSED_ARGUMENT(ebp);
@@ -21,16 +20,18 @@ unsigned int system_call(unsigned int eax, unsigned int ebx, unsigned int ecx, u
 	switch(eax)
 	{
 	case SYSTEM_CALL_EXIT:
-		exit_application(ebx, get_current_task()->task_status_segment.esp0);
-		break;
+		return system_call_exit(ebx);
 	case SYSTEM_CALL_WRITE:
-		system_call_write(ebx, (void const *)ecx, (size_t)edx);
-		break;
+		return system_call_write(ebx, (void const *)ecx, (size_t)edx);
 	default:
 		ERROR(); // Invalid eax
-		break;
+		return -1;
 	}
-	return 0;
+}
+
+unsigned int system_call_exit(unsigned int return_value)
+{
+	return exit_application(return_value, get_current_task()->task_status_segment.esp0);
 }
 
 unsigned int system_call_write(unsigned int file_descriptor, void const *buffer, size_t count)
