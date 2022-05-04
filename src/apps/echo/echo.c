253bbf68ff3e4f5edc98783a142ca3bc85e04e07
TaiseiIto <taisei1212@outlook.jp>
2022-05-04 10:26:50 +0900

    printf is available #58

--- src/apps/echo/echo.c
+++ src/apps/echo/echo.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv)
 {
-	for(int argi = 0; argi < argc; argi++)puts(argv[argi]);
+	for(int argi = 0; argi < argc; argi++)printf("argv[%d] = %s\n", argi, argv[argi]);
 	return 0;
 }
 
