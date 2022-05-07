21a540f6eaae7b43839b94c7a5ec30a37c06d145
TaiseiIto <taisei1212@outlook.jp>
2022-05-07 17:31:42 +0900

    FileStatus #60

--- src/kernel/system_call.c
+++ src/kernel/system_call.c
@@ -14,6 +14,18 @@
 #define STDOUT	0x00000001
 #define STDERR	0x00000002
 
+typedef struct _FileStatus
+{
+	char *file_name;
+	Task *file_opener_task;
+	unsigned int file_descriptor;
+	unsigned int flags;
+	#define SYSTEM_CALL_OPEN_FLAG_READ	0x00000001
+	#define SYSTEM_CALL_OPEN_FLAG_WRITE	0x00000002
+	struct _FileStatus *previous;
+	struct _FileStatus *next;
+} FileStatus;
+
 typedef struct _SystemCallStatus
 {
 	Task *application_task;
@@ -21,13 +33,13 @@ typedef struct _SystemCallStatus
 	struct _SystemCallStatus *next;
 } SystemCallStatus;
 
+FileStatus *file_statuses = NULL;
 SystemCallStatus *system_call_statuses = NULL;
 
 void delete_system_call_status(void);
 SystemCallStatus *get_system_call_status(void);
 int system_call_exit(int return_value);
 unsigned int system_call_open(char const *file_name, unsigned int flags);
-#define SYSTEM_CALL_OPEN_FLAG_READ_ONLY 0x01
 int system_call_write(unsigned int file_descriptor, void const *buffer, size_t count);
 
 void delete_system_call_status(void)
@@ -109,11 +121,53 @@ int system_call_exit(int return_value)
 
 unsigned int system_call_open(char const *file_name, unsigned int flags)
 {
-	Shell *shell = get_current_shell();
-	SystemCallStatus *system_call_status = get_system_call_status();
-	printf_shell(shell, "system_call_status = %p\n", system_call_status);
-	if(!strcmp(file_name, "") && flags == SYSTEM_CALL_OPEN_FLAG_READ_ONLY)printf_shell(shell, "Open root directory!\n");
-	return 0;
+	FileStatus *file_status;
+	if(file_statuses)
+	{
+		unsigned int max_file_descriptor = STDERR; // The max file descriptor opened by the caller application.
+		file_status = file_statuses;
+		do
+		{
+			if(!strcmp(file_status->file_name, file_name)) // Someone opened the same file.
+			{
+				if(file_status->file_opener_task == get_current_task()) // The caller application opened the same file.
+				{
+					// Reuse file_status.
+					file_status->flags |= flags;
+					return file_status->file_descriptor;
+				}
+				else // Another application opened the same file.
+				{
+					if(file_status->flags & SYSTEM_CALL_OPEN_FLAG_WRITE || flags & SYSTEM_CALL_OPEN_FLAG_WRITE)return 0; // Can't open the file with writing mode.
+				}
+			}
+			if(file_status->file_opener_task == get_current_task() && max_file_descriptor < file_status->file_descriptor)max_file_descriptor = file_status->file_descriptor;
+			file_status = file_status->next;
+		} while(file_status != file_statuses);
+		file_status = malloc(sizeof(*file_status));
+		file_status->file_name = malloc(strlen(file_name) + 1);
+		strcpy(file_status->file_name, file_name);
+		file_status->file_opener_task = get_current_task();
+		file_status->file_descriptor = max_file_descriptor + 1;
+		file_status->flags = flags;
+		file_status->previous = file_statuses->previous;
+		file_status->next = file_statuses;
+		file_statuses->previous->next = file_status;
+		file_statuses->previous = file_status;
+	}
+	else
+	{
+		file_status = malloc(sizeof(*file_status));
+		file_status->file_name = malloc(strlen(file_name) + 1);
+		strcpy(file_status->file_name, file_name);
+		file_status->file_opener_task = get_current_task();
+		file_status->file_descriptor = STDERR + 1;
+		file_status->flags = flags;
+		file_status->previous = file_status;
+		file_status->next = file_status;
+		file_statuses = file_status;
+	}
+	return file_status->file_descriptor;
 }
 
 int system_call_write(unsigned int file_descriptor, void const *buffer, size_t count)
