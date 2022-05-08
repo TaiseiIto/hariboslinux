0274f1c220ddbc5c2e20b6afad0aa11368e42e50
TaiseiIto <taisei1212@outlook.jp>
2022-05-08 11:00:05 +0900

    ls #60

--- src/apps/ls/ls.c
+++ src/apps/ls/ls.c
@@ -1,10 +1,11 @@
 #include "stdio.h"
+#include "string.h"
 
 int main(void)
 {
 	FileInformation file_information;
 	unsigned int file_descriptor = fopen("", "r");
-	while(fread(&file_information, sizeof(file_information), 1, file_descriptor))printf("%.*s.%.*s", sizeof(file_information.name), file_information.name, sizeof(file_information.extension), file_information.extension);
+	while(fread(&file_information, sizeof(file_information), 1, file_descriptor) && strcmp(file_information.name, ""))printf("%.*s.%.*s\n", sizeof(file_information.name), file_information.name, sizeof(file_information.extension), file_information.extension);
 	fclose(file_descriptor);
 	return 0;
 }
