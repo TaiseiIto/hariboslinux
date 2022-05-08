f38f325ca905a1499695301ee604f83e5b91033c
TaiseiIto <taisei1212@outlook.jp>
2022-05-08 15:41:23 +0900

    free #65

--- src/apps/free/free.c
+++ src/apps/free/free.c
@@ -2,7 +2,11 @@
 
 int main(void)
 {
-	printf("Hello, World!\n");
+	unsigned int free_memory_size;
+	unsigned int file_descriptor = fopen("free.dev", "r");
+	fread(&free_memory_size, sizeof(free_memory_size), 1, file_descriptor);
+	printf("free memory space %#010.8x bytes\n", free_memory_size);
+	fclose(file_descriptor);
 	return 0;
 }
 
