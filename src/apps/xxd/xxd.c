51d66a3a2af139226b527bdee1ccfbd6a6d3b967
TaiseiIto <taisei1212@outlook.jp>
2022-05-08 13:45:55 +0900

    xxd #62

--- src/apps/xxd/xxd.c
+++ src/apps/xxd/xxd.c
@@ -6,7 +6,7 @@ int main(int argc, char const * const * const argv)
 {
 	if(2 <= argc)
 	{
-		char buffer[0x10];
+		unsigned char buffer[0x10];
 		unsigned int file_descriptor = fopen(argv[1], "r");
 		size_t read_size;
 		while((read_size = fread(buffer, sizeof(buffer[0]), _countof(buffer), file_descriptor)))
