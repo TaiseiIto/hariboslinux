41f3d7a793b83898ad5d50aeabbc8b728956fda5
TaiseiIto <taisei1212@outlook.jp>
2022-05-08 13:20:29 +0900

    cat #61

--- src/apps/cat/cat.c
+++ src/apps/cat/cat.c
@@ -8,7 +8,8 @@ int main(int argc, char const * const * const argv)
 	{
 		char buffer[0x200];
 		unsigned int file_descriptor = fopen(argv[1], "r");
-		while(fread(buffer, sizeof(buffer[0]), _countof(buffer), file_descriptor))printf("%.*s", _countof(buffer), buffer);
+		size_t read_size;
+		while((read_size = fread(buffer, sizeof(buffer[0]), _countof(buffer), file_descriptor)))printf("%.*s", read_size, buffer);
 		fclose(file_descriptor);
 		return 0;
 	}
