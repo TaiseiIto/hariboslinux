c37f6a58a97f2c07ba7fadeeb40dddb4b1a6f325
TaiseiIto <taisei1212@outlook.jp>
2022-06-25 21:44:56 +0900

    calc #83

--- src/apps/calc/calc.c
+++ src/apps/calc/calc.c
@@ -1,8 +1,30 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+char *combine_argv(int argc, char const * const * const argv);
 
 int main(int argc, char const * const * const argv)
 {
-	for(int argi = 0; argi < argc; argi++)printf("argv[%d] = %s\n", argi, argv[argi]);
+	char *input_string = combine_argv(argc - 1, argv + 1);
+	printf("%s\n", input_string);
+	free(input_string);
 	return 0;
+}
+
+char *combine_argv(int argc, char const * const * const argv)
+{
+	char *combined_string;
+	char *writer;
+	unsigned int sum_length = 0;
+	for(int argi = 0; argi < argc; argi++)sum_length += strlen(argv[argi]);
+	combined_string = malloc(sum_length + 1);
+	writer = combined_string;
+	for(int argi = 0; argi < argc; argi++)
+	{
+		strcpy(writer, argv[argi]);
+		writer += strlen(argv[argi]);
+	}
+	return combined_string;
 }
 
