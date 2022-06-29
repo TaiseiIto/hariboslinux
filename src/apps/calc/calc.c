79d95d420dcaa8aa5836f150c130183b472b028d
TaiseiIto <taisei1212@outlook.jp>
2022-06-29 21:41:59 +0900

    debug 1.2*3.4+5.6/7.8 #83

--- src/apps/calc/calc.c
+++ src/apps/calc/calc.c
@@ -679,6 +679,8 @@ Symbols syntactic_analysis(Symbols symbols)
 				new_symbol->component.term.factor = symbol;
 				new_symbol->string.initial = symbol->string.initial;
 				new_symbol->string.length = symbol->string.length;
+				new_symbol->previous = symbol->previous;
+				new_symbol->next = symbol->next->next;
 				if(new_symbol->previous)new_symbol->previous->next = new_symbol;
 				if(new_symbol->next)new_symbol->next->previous = new_symbol;
 				if(symbols.first_symbol == symbol)symbols.first_symbol = new_symbol;
