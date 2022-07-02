f4a3d861687b4f232b6e22b9cded0fb1149eeae1
TaiseiIto <taisei1212@outlook.jp>
2022-07-02 13:47:52 +0900

    GPG setting #87

--- git/gitconfig.sh
+++ git/gitconfig.sh
@@ -2,7 +2,8 @@
 
 # This program gives the push access right.
 # Only the developer can execute it.
-# Put the private key "/root/hariboslinux/ssh/github" before the execution.
+# Put the ssh private key "/root/hariboslinux/ssh/github" before the execution.
+# Put the gpg directory "/root/.gnupg" before the execution.
 
 currentdir=$(pwd)
 cd $(dirname $0)
@@ -12,6 +13,8 @@ echo -n "Your GitHub name:"
 read name
 git config --global user.email $email
 git config --global user.name $name
+git config --global user.signingkey 5F1ABB3B591CCA48
+git config --global --add commit.gpgsign true
 git remote set-url origin git@github.com:TaiseiIto/hariboslinux.git
 cat gitconfig >> /root/.gitconfig
 cat ../ssh/config >> /root/.ssh/config
