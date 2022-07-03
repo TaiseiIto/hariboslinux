6fb5774af85f6ee5429308358ef5d0ec54449611
TaiseiIto <taisei1212@outlook.jp>
2022-07-03 19:17:24 +0900

    print double #83

--- src/libs/chain_string.c
+++ src/libs/chain_string.c
@@ -2,6 +2,7 @@
 #include "common.h"
 #include "io.h"
 #include "limits.h"
+#include "math.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
@@ -195,6 +196,8 @@ ChainString *create_caller_format_chain_string(unsigned int format_arg_pos)
 			if(arg_size == 8)arg.unsigned_ints[1] = get_caller_variadic_arg(arg_pos++);
 			switch(*++format)
 			{
+				double integer_part;
+				double fractional_part;
 			case 'c':
 			case 'C':
 				insert_char_back(output_chain_string, output_chain_string->last_character, arg.chars[0]);
@@ -267,6 +270,19 @@ ChainString *create_caller_format_chain_string(unsigned int format_arg_pos)
 					output_length++;
 					sign_character = output_chain_string->last_character;
 					arg.double_floating_point *= -1.0;
+				}
+				integer_part = floor(arg.double_floating_point);
+				fractional_part = arg.double_floating_point - integer_part;
+				for(; 1.0 <= integer_part; integer_part = floor(integer_part / 10.0))
+				{
+					insert_char_back(output_chain_string, sign_character, (char)fmod(integer_part, 10.0) + '0');
+					output_length++;
+				}
+				insert_char_back(output_chain_string, output_chain_string->last_character, '.');
+				for(; precision; precision--)
+				{
+					fractional_part = fmod(10.0 * fractional_part, 10.0);
+					insert_char_back(output_chain_string, output_chain_string->last_character, (char)floor(fmod(fractional_part, 10.0)) + '0');
 				}
 				break;
 			case 'n':
