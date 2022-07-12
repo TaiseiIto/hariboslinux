c5fc722e2c535978117353d550e6b9d861566efc
TaiseiIto <taisei1212@outlook.jp>
2022-07-12 20:06:18 +0900

    mandel #95

---
+++ src/apps/mandel/mandel.c
@@ -0,0 +1,25 @@
+#include "dev.h"
+#include "stdio.h"
+
+int main(void)
+{
+	#define WINDOW_EXISTS 0x01
+	unsigned char flags = WINDOW_EXISTS;
+	unsigned int window = create_window("mandelbrot", 0x0200, 0x0200, 0x0200, 0x0200);
+	while(flags & WINDOW_EXISTS)
+	{
+		ApplicationEvent application_event = dequeue_application_event();
+		switch(application_event.type)
+		{
+		case APPLICATION_EVENT_TYPE_NOTHING:
+			hlt_application();
+			break;
+		case APPLICATION_EVENT_TYPE_WINDOW_DELETION_RESPONSE:
+			if(application_event.event_union.window_deletion_response_event.window == window)flags &= ~WINDOW_EXISTS;
+			break;
+		}
+		process_event();
+	}
+	return 0;
+}
+
