391aa43986911327eec090ed76356913e1ea0221
TaiseiIto <taisei1212@outlook.jp>
2022-09-12 10:53:47 +0000

    test_os.s

--- test_os/test_os.s
+++ test_os/test_os.s
@@ -1,14 +1,14 @@
 	.code16
 	.text
 main:
-	fninit
-	fld1
-	fldl2t
-	fldl2e
-	fldpi
-	fldlg2
-	fldln2
-	fldz
+	fninit	# Initialize the FPU
+	fld1	# Push 1.0
+	fldl2t	# Push log 2 10
+	fldl2e	# Push log 2 e
+	fldpi	# Push pi
+	fldlg2	# Push log 10 2
+	fldln2	# Push log e 2
+	fldz	# Push 0.0
 loop:
 	hlt
 	jmp loop
