70115091a98434a3e1806c6cea9279cf3c8f446b
TaiseiIto <taisei1212@outlook.jp>
2022-09-17 14:54:31 +0900

    fpu_test #105

--- fpu_test/fpu_test.s
+++ fpu_test/fpu_test.s
@@ -1,6 +1,14 @@
 	.globl	main
 	.text
 main:
+	fninit	# Initialize the FPU
+	fld1	# Push 1.0
+	fldl2t	# Push log 2 10
+	fldl2e	# Push log 2 e
+	fldpi	# Push pi
+	fldlg2	# Push log 10 2
+	fldln2	# Push log e 2
+	fldz	# Push 0.0
 	movq	$0x000000000000003c,	%rax
 	xorq	%rdi,	%rdi
 	syscall
