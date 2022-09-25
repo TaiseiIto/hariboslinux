9c18f0fafc3fc4f6939727533b9e55a633853a79
TaiseiIto <taisei1212@outlook.jp>
2022-09-25 18:35:28 +0900

    validate log(e,-1) #83

--- src/apps/calc/calc.c
+++ src/apps/calc/calc.c
@@ -2880,7 +2880,7 @@ Symbols syntactic_analysis(Symbols symbols)
 		case plus:
 			if(symbol->next && symbol->next->type == factor)
 			{
-				if(symbol->previous && symbol->previous->type != left_parenthesis)break;
+				if(symbol->previous && symbol->previous->type != left_parenthesis && symbol->previous->type != comma)break;
 				if(symbol->next->next && symbol->next->next->type == asterisk)break;
 				if(symbol->next->next && symbol->next->next->type == slash)break;
 				// <term> ::= <minus> <factor> | <plus> <factor>
