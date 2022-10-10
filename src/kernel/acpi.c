154ad532b3b667df877bc9a788c8550cb7a1d4ec
TaiseiIto <taisei1212@outlook.jp>
2022-10-10 21:59:39 +0900

    ACPI table checksum #85

--- src/kernel/acpi.c
+++ src/kernel/acpi.c
@@ -1,12 +1,27 @@
 #include "acpi.h"
 #include "serial.h"
+#include "stdbool.h"
 #include "string.h"
+
+bool acpi_table_is_correct(ACPITableHeader const *header);
+
+bool acpi_table_is_correct(ACPITableHeader const *header)
+{
+	unsigned char sum = 0;
+	for(unsigned char const *byte = (unsigned char const *)header; byte != (unsigned char const *)((unsigned int)header + header->length); byte++)sum += *byte;
+	if(sum)
+	{
+		ERROR(); // It's an incorrect ACPI table!
+		return false;
+	}
+	else return true;
+}
 
 ACPITableHeader const *find_sdt_header(char const *signature)
 {
 	ACPITableHeader const * const *sdt_headers = get_sdt_headers();
 	unsigned int num_of_sdt_headers = get_num_of_sdt_headers();
-	for(unsigned int sdt_header_index = 0; sdt_header_index < num_of_sdt_headers; sdt_header_index++)if(!strncmp(sdt_headers[sdt_header_index]->signature, signature, sizeof(sdt_headers[sdt_header_index]->signature)))return sdt_headers[sdt_header_index];
+	for(unsigned int sdt_header_index = 0; sdt_header_index < num_of_sdt_headers; sdt_header_index++)if(!strncmp(sdt_headers[sdt_header_index]->signature, signature, sizeof(sdt_headers[sdt_header_index]->signature)) && acpi_table_is_correct(sdt_headers[sdt_header_index]))return sdt_headers[sdt_header_index];
 	ERROR(); // SDT that has a specified signature is not found.
 	return NULL;
 }
@@ -26,7 +41,13 @@ MemoryRegionDescriptor get_acpi_memory_region_descriptor(void)
 
 ACPITableHeader const *get_rsdt_header(void)
 {
-	return (ACPITableHeader const *)(unsigned int)get_acpi_memory_region_descriptor().base;
+	ACPITableHeader const *rsdt_header = (ACPITableHeader const *)(unsigned int)get_acpi_memory_region_descriptor().base;
+	if(acpi_table_is_correct(rsdt_header))return rsdt_header;
+	else
+	{
+		ERROR(); // RSDT is incorrect!
+		return NULL;
+	}
 }
 
 ACPITableHeader const * const *get_sdt_headers(void)
