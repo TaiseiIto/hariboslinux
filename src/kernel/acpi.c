f392946f73923806a1e561d86ed0c5a1eacb232a
TaiseiIto <taisei1212@outlook.jp>
2022-11-06 22:20:08 +0900

    get_aml_symbol_common_ancestor #85

--- src/kernel/acpi.c
+++ src/kernel/acpi.c
@@ -5021,7 +5021,10 @@ AMLSymbol *analyse_aml_term_obj(AMLSymbol *parent, AMLSubstring aml)
 	// Undefined method invocation arglist addition
 	switch(*aml.initial)
 	{
-		AMLSymbol const *method_invocation;
+		AMLSymbol *method_invocation;
+		AMLSymbol *term_arg_list_root;
+		AMLSymbol *term_arg_list_tail;
+		char const *invoked_method_name;
 	case AML_BYTE_ARG_0_OP:
 	case AML_BYTE_ARG_1_OP:
 	case AML_BYTE_ARG_2_OP:
@@ -5046,12 +5049,73 @@ AMLSymbol *analyse_aml_term_obj(AMLSymbol *parent, AMLSubstring aml)
 	case AML_BYTE_STRING_PREFIX:
 	case AML_BYTE_WORD_PREFIX:
 	case AML_BYTE_ZERO_OP:
-		ERROR();
 		printf_serial("Extra term arg by undefined method invocation.\n");
 		method_invocation = get_aml_previous_method_invocation(term_obj, NULL);
-		if(method_invocation)
+		if(!method_invocation)
 		{
-			printf_serial("Previous method invocation \"%s\"\n", method_invocation->component.method_invocation.name_string->component.name_string.string);
+			ERROR(); // method_invocation is not found.
+			break;
+		}
+		invoked_method_name = method_invocation->component.method_invocation.name_string->component.name_string.string;
+		if(get_aml_method(invoked_method_name, term_obj, NULL))
+		{
+			ERROR(); // The method is defined.
+			break;
+		}
+		if(method_invocation->string.initial + method_invocation->string.length != term_obj->string.initial)
+		{
+			ERROR(); // method_invocation is not adjacent with term_obj on AML byte code.
+			break;
+		}
+		printf_serial("Previous method invocation \"%s\"\n", invoked_method_name);
+		term_arg_list_root = method_invocation->component.method_invocation.term_arg_list;
+		term_arg_list_tail = term_arg_list_root;
+		while(term_arg_list_tail->component.term_arg_list.term_arg_list)term_arg_list_tail = term_arg_list_tail->component.term_arg_list.term_arg_list;
+		printf_serial("term_arg_list_root = %p\n", term_arg_list_root);
+		printf_serial("term_arg_list_tail = %p\n", term_arg_list_tail);
+		while
+		(
+			*aml.initial == AML_BYTE_ARG_0_OP ||
+			*aml.initial == AML_BYTE_ARG_1_OP ||
+			*aml.initial == AML_BYTE_ARG_2_OP ||
+			*aml.initial == AML_BYTE_ARG_3_OP ||
+			*aml.initial == AML_BYTE_ARG_4_OP ||
+			*aml.initial == AML_BYTE_ARG_5_OP ||
+			*aml.initial == AML_BYTE_ARG_6_OP ||
+			*aml.initial == AML_BYTE_BUFFER_OP ||
+			*aml.initial == AML_BYTE_BYTE_PREFIX ||
+			*aml.initial == AML_BYTE_DWORD_PREFIX ||
+			*aml.initial == AML_BYTE_LOCAL_0_OP ||
+			*aml.initial == AML_BYTE_LOCAL_1_OP ||
+			*aml.initial == AML_BYTE_LOCAL_2_OP ||
+			*aml.initial == AML_BYTE_LOCAL_3_OP ||
+			*aml.initial == AML_BYTE_LOCAL_4_OP ||
+			*aml.initial == AML_BYTE_LOCAL_5_OP ||
+			*aml.initial == AML_BYTE_LOCAL_6_OP ||
+			*aml.initial == AML_BYTE_LOCAL_7_OP ||
+			*aml.initial == AML_BYTE_ONE_OP ||
+			*aml.initial == AML_BYTE_ONES_OP ||
+			*aml.initial == AML_BYTE_QWORD_PREFIX ||
+			*aml.initial == AML_BYTE_STRING_PREFIX ||
+			*aml.initial == AML_BYTE_WORD_PREFIX ||
+			*aml.initial == AML_BYTE_ZERO_OP
+		)
+		{
+			AMLSymbol *new_term_arg_list = malloc(sizeof(*new_term_arg_list));
+			unsigned int new_term_arg_length;
+			new_term_arg_list->parent = term_arg_list_tail;
+			new_term_arg_list->string.initial = aml.initial;
+			new_term_arg_list->string.length = 0;
+			new_term_arg_list->type = aml_term_arg_list;
+			new_term_arg_list->component.term_arg_list.term_arg = NULL;
+			new_term_arg_list->component.term_arg_list.term_arg_list = NULL;
+			new_term_arg_list->component.term_arg_list.term_arg = analyse_aml_term_arg(new_term_arg_list, aml);
+			new_term_arg_length = new_term_arg_list->component.term_arg_list.term_arg->string.length;
+			term_arg_list_tail->component.term_arg_list.term_arg_list = new_term_arg_list;
+			for(AMLSymbol *extended_aml_symbol = new_term_arg_list; extended_aml_symbol != get_aml_symbol_common_ancestor(term_obj, method_invocation)->parent; extended_aml_symbol = extended_aml_symbol->parent)extended_aml_symbol->string.length += new_term_arg_length;
+			aml.initial += new_term_arg_length;
+			aml.length -= new_term_arg_length;
+			term_arg_list_tail = new_term_arg_list;
 		}
 		break;
 	default:
@@ -6175,21 +6239,21 @@ AMLSymbol const *get_aml_method(char const *method_name, AMLSymbol const *aml_sy
 	else return NULL;
 }
 
-AMLSymbol const *get_aml_previous_method_invocation(AMLSymbol const *aml_symbol, AMLSymbol const *searched)
+AMLSymbol *get_aml_previous_method_invocation(AMLSymbol *aml_symbol, AMLSymbol *searched)
 {
-	AMLSymbol const *method_invocation = NULL;
-	AMLSymbol const *method_invocation_in_def_device = NULL;
-	AMLSymbol const *method_invocation_in_def_else = NULL;
-	AMLSymbol const *method_invocation_in_def_if_else = NULL;
-	AMLSymbol const *method_invocation_in_def_method = NULL;
-	AMLSymbol const *method_invocation_in_def_while = NULL;
-	AMLSymbol const *method_invocation_in_expression_opcode = NULL;
-	AMLSymbol const *method_invocation_in_name_space_modifier_obj = NULL;
-	AMLSymbol const *method_invocation_in_named_obj = NULL;
-	AMLSymbol const *method_invocation_in_object = NULL;
-	AMLSymbol const *method_invocation_in_term_list = NULL;
-	AMLSymbol const *method_invocation_in_term_obj = NULL;
-	AMLSymbol const *method_invocation_in_predicate = NULL;
+	AMLSymbol *method_invocation = NULL;
+	AMLSymbol *method_invocation_in_def_device = NULL;
+	AMLSymbol *method_invocation_in_def_else = NULL;
+	AMLSymbol *method_invocation_in_def_if_else = NULL;
+	AMLSymbol *method_invocation_in_def_method = NULL;
+	AMLSymbol *method_invocation_in_def_while = NULL;
+	AMLSymbol *method_invocation_in_expression_opcode = NULL;
+	AMLSymbol *method_invocation_in_name_space_modifier_obj = NULL;
+	AMLSymbol *method_invocation_in_named_obj = NULL;
+	AMLSymbol *method_invocation_in_object = NULL;
+	AMLSymbol *method_invocation_in_term_list = NULL;
+	AMLSymbol *method_invocation_in_term_obj = NULL;
+	AMLSymbol *method_invocation_in_predicate = NULL;
 	if(!aml_symbol)return NULL;
 	switch(aml_symbol->type)
 	{
@@ -6416,6 +6480,12 @@ unsigned short get_aml_s5_pm1_cnt_slp_typ(AMLSymbol const *aml_symbol)
 		return 0;
 	}
 	return (unsigned short)pm1b_cnt_slp_typ << 8 | (unsigned short)pm1a_cnt_slp_typ;
+}
+
+AMLSymbol *get_aml_symbol_common_ancestor(AMLSymbol *aml_symbol_a, AMLSymbol *aml_symbol_b)
+{
+	for(AMLSymbol *aml_symbol_a_ancestor = aml_symbol_a; aml_symbol_a_ancestor; aml_symbol_a_ancestor = aml_symbol_a_ancestor->parent)for(AMLSymbol *aml_symbol_b_ancestor = aml_symbol_b; aml_symbol_b_ancestor; aml_symbol_b_ancestor = aml_symbol_b_ancestor->parent)if(aml_symbol_a_ancestor == aml_symbol_b_ancestor)return aml_symbol_a_ancestor;
+	return NULL; // There is no common ancestor.
 }
 
 unsigned int get_aml_symbol_depth(AMLSymbol const *aml_symbol)
