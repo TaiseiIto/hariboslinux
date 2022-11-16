4588c3aae7ab43798ccacd800ccee8a2d1d7a7f3
TaiseiIto <taisei1212@outlook.jp>
2022-11-16 20:19:22 +0900

    anlysaml #85

--- src/apps/anlysaml/anlysaml.c
+++ src/apps/anlysaml/anlysaml.c
@@ -1,7 +1,21 @@
 #include "dev.h"
+#include "stdio.h"
+#include "string.h"
 
-int main(void)
+int main(int argc, char const * const * const argv)
 {
+	if(argc <= 1)
+	{
+		printf("%s : Satisfy a file name.\n", argv[0]);
+		return -1;
+	}
+	char const *aml_file_name = argv[1];
+	char const *aml_file_name_prefix = aml_file_name;
+	unsigned int aml_file_name_prefix_length = (unsigned int)strchr(aml_file_name_prefix, '.') - (unsigned int)aml_file_name_prefix;
+	char const *aml_file_name_suffix = aml_file_name_prefix + aml_file_name_prefix_length + 1;
+	unsigned int aml_file_name_suffix_length = strlen(aml_file_name_suffix);
+	printf("aml_file_name_prefix = %.*s\n", aml_file_name_prefix_length, aml_file_name_prefix);
+	printf("aml_file_name_suffix = %.*s\n", aml_file_name_suffix_length, aml_file_name_suffix);
 	return 0;
 }
 
